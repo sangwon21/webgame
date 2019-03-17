dec76a48614f19e8ef043bf503ce2947ea8ede21
Sangwon <junothyme@gmail.com>
2019-03-17 15:16:15 +0900

    Works on Tictacto 2

--- tictacto.js
+++ tictacto.js
@@ -1,5 +1,6 @@
 let body = document.body;
 let table = document.createElement("table");
+let result = document.createElement("div");
 
 let columns = [];
 let rows = [];
@@ -9,12 +10,66 @@ const callback = e => {
 
   const column = columns[row].indexOf(e.target);
 
-  if (columns[row][column].textContent !== '') {
-    console.log("empty space");
+  if (columns[row][column].textContent !== "") {
+    console.log("not empty space");
   } else {
     columns[row][column].textContent = turn;
-    if (turn === "X") turn = "O";
-    else turn = "X";
+
+    var fullFlag = false;
+
+    if (
+      columns[row][0].textContent === turn &&
+      columns[row][1].textContent === turn &&
+      columns[row][2].textContent === turn
+    ) {
+      console.log("가로줄");
+      fullFlag = true;
+    }
+
+    if (
+      columns[0][column].textContent === turn &&
+      columns[1][column].textContent === turn &&
+      columns[2][column].textContent === turn
+    ) {
+      console.log("세로줄");
+      fullFlag = true;
+    }
+    if (row - column === 0) {
+      if (
+        columns[0][0].textContent === turn &&
+        columns[1][1].textContent === turn &&
+        columns[2][2].textContent === turn
+      ) {
+        console.log("대각선1");
+        fullFlag = true;
+      }
+    }
+
+    if (Math.abs(row - column) === 2) {
+      if (
+        columns[0][2].textContent === turn &&
+        columns[1][1].textContent === turn &&
+        columns[2][0].textContent === turn
+      ) {
+        console.log("대각선2");
+        fullFlag = true;
+      }
+    }
+  }
+  console.log(`row: ${row}, column:${column}`);
+  console.log(`fullFlag is ${fullFlag}`);
+  if (fullFlag) {
+    result.textContent = `${turn} won!!`;
+    columns.forEach(column =>
+      column.forEach(row => {
+        row.textContent = "";
+      })
+    );
+  } else {
+    if (turn === "X") {
+      turn = "O";
+      console.log("turn change");
+    } else turn = "X";
   }
 };
 
@@ -32,4 +87,5 @@ for (i = 1; i <= 3; i++) {
   table.appendChild(row);
 }
 body.appendChild(table);
+body.appendChild(result);
 //console.log(columns, rows);
