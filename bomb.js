a8f4328465347cf8a64155e5340b77a190ab66ef
Sangwon <junothyme@gmail.com>
2019-03-23 18:17:01 +0900

    Picking Bomb Location

--- bomb.js
+++ bomb.js
@@ -3,19 +3,47 @@ document.querySelector("#exec").addEventListener("click", () => {
   let ver = parseInt(document.querySelector("#ver").value);
   let mine = parseInt(document.querySelector("#mine").value);
 
-  let dataSet=[];
+  let dataSet = [];
   let tbody = document.querySelector("#table tbody");
 
-  for(let i = 0; i < ver; i++){
-      let arr = [];
-      let tr = document.createElement("tr");
-      dataSet.push(arr);
-      for(let j = 0; j < hor; j++){
-          let td = document.createElement("td");
-          arr.push(1);
-          tr.appendChild(td);
-          td.textContent = "1";
-      }
-      tbody.appendChild(tr);
+  // Picking bombs location
+
+  let candidate = Array(hor * ver)
+    .fill()
+    .map((_, idx) => idx + 1);
+
+  let shuffle = [];
+
+  while (candidate.length > 80) {
+    let pickedNum = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
+    shuffle.push(pickedNum);
   }
+
+  console.log("shuffle:");
+  console.log(shuffle);
+
+  for (let i = 0; i < ver; i++) {
+    let arr = [];
+    let tr = document.createElement("tr");
+    dataSet.push(arr);
+    for (let j = 0; j < hor; j++) {
+      let td = document.createElement("td");
+      arr.push(1);
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  }
+
+  for (let i = 0; i < shuffle.length; i++) {
+    let vertical = Math.floor(shuffle[i] / 10);
+    let horizon = shuffle[i] % 10;
+    console.log(vertical, horizon);
+    tbody.children[vertical].children[horizon].textContent = "X";
+    dataSet[vertical][horizon] = "X";
+  }
+
+  console.log(dataSet);
 });
