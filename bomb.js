79eb81891b8fd55c86bdc3b533d66c644ad9245f
Sangwon <junothyme@gmail.com>
2019-03-24 21:55:04 +0900

    Recursive on Finding Zeroes

--- bomb.js
+++ bomb.js
@@ -32,7 +32,7 @@ document.querySelector("#exec").addEventListener("click", () => {
     dataSet.push(arr);
     for (let j = 0; j < hor; j++) {
       let td = document.createElement("td");
-
+      arr.push(0);
       td.addEventListener("contextmenu", e => {
         e.preventDefault();
         let parentTr = e.currentTarget.parentNode;
@@ -60,7 +60,84 @@ document.querySelector("#exec").addEventListener("click", () => {
         }
       });
 
-      arr.push(1);
+      td.addEventListener("click", e => {
+        let parentTr = e.currentTarget.parentNode;
+        let parentTbody = e.currentTarget.parentNode.parentNode;
+        let kan = Array.prototype.indexOf.call(
+          parentTr.children,
+          e.currentTarget
+        );
+        let jul = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+        e.currentTarget.classList.add("opened");
+
+        if (dataSet[jul][kan] === "X") {
+          e.currentTarget.textContent = "펑";
+        } else {
+          dataSet[jul][kan] = 1;
+          let around = [dataSet[jul][kan - 1], dataSet[jul][kan + 1]];
+          //console.log(dataSet[jul][kan - 1], dataSet[jul][kan + 1])
+          if (dataSet[jul - 1]) {
+            around = around.concat(
+              dataSet[jul - 1][kan - 1],
+              dataSet[jul - 1][kan],
+              dataSet[jul - 1][kan + 1]
+            );
+          }
+          if (dataSet[jul + 1]) {
+            around = around.concat(
+              dataSet[jul + 1][kan - 1],
+              dataSet[jul + 1][kan],
+              dataSet[jul + 1][kan + 1]
+            );
+          }
+          let aroundBombNum = around.filter(element => {
+            return element === "X";
+          }).length;
+          e.currentTarget.textContent = aroundBombNum;
+          if (aroundBombNum === 0) {
+            let aroundKan = [];
+            if (tbody.children[jul - 1]) {
+              aroundKan = aroundKan.concat([
+                tbody.children[jul - 1].children[kan - 1],
+                tbody.children[jul - 1].children[kan],
+                tbody.children[jul - 1].children[kan + 1]
+              ]);
+            }
+
+            aroundKan = aroundKan.concat([
+              tbody.children[jul].children[kan - 1],
+
+              tbody.children[jul].children[kan + 1]
+            ]);
+
+            if (tbody.children[jul + 1]) {
+              aroundKan = aroundKan.concat([
+                tbody.children[jul + 1].children[kan - 1],
+                tbody.children[jul + 1].children[kan],
+                tbody.children[jul + 1].children[kan + 1]
+              ]);
+            }
+            //console.log(aroundKan);
+            aroundKan
+              .filter(v => !!v)
+              .forEach(next => {
+                let parentTr = next.parentNode;
+                let parentTbody = next.parentNode.parentNode;
+                let kan = Array.prototype.indexOf.call(parentTr.children, next);
+                let jul = Array.prototype.indexOf.call(
+                  parentTbody.children,
+                  parentTr
+                );
+                console.log(jul, kan);
+                if (dataSet[jul][kan] !== 1) {
+                  next.click();
+                }
+              });
+          }
+          //console.log(around);
+        }
+      });
+
       tr.appendChild(td);
     }
     tbody.appendChild(tr);
